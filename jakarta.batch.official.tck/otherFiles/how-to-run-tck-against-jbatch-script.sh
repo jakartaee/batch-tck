0c48e15c7350e56a7594735a0066ebf300165798
Scott Kurz <skurz@us.ibm.com>
2020-07-25 10:47:08 -0400

    Add Java 11 instructions; clean important dependencies from local .m2
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- jakarta.batch.official.tck/otherFiles/how-to-run-tck-against-jbatch-script.sh
+++ jakarta.batch.official.tck/otherFiles/how-to-run-tck-against-jbatch-script.sh
@@ -15,27 +15,29 @@ set -x
 ################
 # OK TO MODIFY
 ################
+echo
+echo --------------------------------
+echo Begin setup using Java 8
+echo --------------------------------
+echo
 
 # 1. Root location of TCK execution - Also useful for holding this script itself, and its output logs
 TCK_HOME_DIR=~/jkbatch
 
-# 2. Point to JAVA_HOME so that the signature test command below can find the runtime JAR (rt.jar):
-#export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-11-openj9-amd64/
+# 2. Point to JAVA_HOME so that the signature test command below can find the runtime JAR (rt.jar) for
+# Java 8 and the modules for Java 11.
+#
 export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-11-openj9
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.212.b04-0.el7_6.x86_64/
 
 
 
+# 3. Copy required JARs obtained via other mechanisms
 #------------------------------------------------------------------------------------------------------
 # NOTE: Since these are Maven coordinates of already-released artifacts, we take the shortcut of
-# priming the local Maven repository, to be copied locally below.  One way of doing this, since the TCK
-# project mvn build includes modules executing the TCK against 'jbatch', is to clone and execute the
-# TCK project like:
-#
-#     git clone git@github.com:eclipse-ee4j/batch-tck.git; cd batch-tck
-#     mvn clean install -DskipSigTests=true
+# priming the local Maven repository, to be copied locally below.  To make sure we don't have a stale
+# local copy, we delete them from the local ~/.m2/repository first.
 #------------------------------------------------------------------------------------------------------
-# 3. Copy required JARs obtained via other mechanisms
 REQUIRED_JARS="\
  /home/ibmadmin/.m2/repository/org/apache/derby/derby/10.10.1.1/derby-10.10.1.1.jar \
  /home/ibmadmin/.m2/repository/com/ibm/jbatch/com.ibm.jbatch.container/2.0.0-M6/com.ibm.jbatch.container-2.0.0-M6.jar \
@@ -43,6 +45,16 @@ REQUIRED_JARS="\
  /home/ibmadmin/.m2/repository/net/java/sigtest/sigtestdev/3.0-b12-v20140219/sigtestdev-3.0-b12-v20140219.jar \
  /home/ibmadmin/.m2/repository/jakarta/batch/jakarta.batch-api/2.0.0-M6/jakarta.batch-api-2.0.0-M6.jar \
 "
+
+rm $REQUIRED_JARS
+for f in "$REQUIRED_JARS"; do ls -l $f ; done
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=org.apache.derby:derby:10.10.1.1 -DrepoUrl=https://repo1.maven.org/maven2/
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=jakarta.batch:jakarta.batch-api:2.0.0-M6 -DrepoUrl=https://repo1.maven.org/maven2/
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=net.java.sigtest:sigtestdev:3.0-b12-v20140219 -DrepoUrl=https://repo1.maven.org/maven2/
+# Gets com.ibm.jbatch.spi as well.
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.2:get -Dartifact=com.ibm.jbatch:com.ibm.jbatch.container:2.0.0-M6 -DrepoUrl=https://repo1.maven.org/maven2/
+
 
 #--------------------------------------------------
 # Show some basic info about the OS, JDK/JRE, etc.
@@ -56,17 +68,23 @@ ls -l $JAVA_HOME/jre/lib/rt.jar
 $JAVA_HOME/bin/java -version
 
 #############################################
-# DON'T CHANGE
+# DON'T CHANGE remainder of Java 8 portion
 # (except comment out official URL override
 #  when testing a staged copy of TCK)
+#
+# When you get to the Java 11 portion, you
+#  can set the JAVA_HOME to a different JDK
+#  location.
 #############################################
+
+TCK_ARTIFACT_ID=jakarta.batch.official.tck-2.0.0-M4
 
 #
 # STAGED
 #
-TCK_DOWNLOAD_URL=https://download.eclipse.org/jakartabatch/tck/eftl/jakarta.batch.official.tck-2.0.0-M4.zip
-TCK_DOWNLOAD_URL=https://oss.sonatype.org/content/repositories/staging/jakarta/batch/jakarta.batch.official.tck/2.0.0-M4/jakarta.batch.official.tck-2.0.0-M4.zip
-TCK_DOWNLOAD_URL=https://oss.sonatype.org/service/local/repositories/jakartabatch-1030/content/jakarta/batch/jakarta.batch.official.tck/2.0.0-M4/jakarta.batch.official.tck-2.0.0-M4.zip
+TCK_DOWNLOAD_URL=https://download.eclipse.org/jakartabatch/tck/eftl/$TCK_ARTIFACT_ID.zip
+TCK_DOWNLOAD_URL=https://oss.sonatype.org/content/repositories/staging/jakarta/batch/jakarta.batch.official.tck/2.0.0-M4/$TCK_ARTIFACT_ID.zip
+TCK_DOWNLOAD_URL=https://repo1.maven.org/maven2/jakarta/batch/jakarta.batch.official.tck/2.0.0-M4/$TCK_ARTIFACT_ID.zip
 
 #
 # OFFICIAL (will look like this)
@@ -103,20 +121,29 @@ openssl dgst -sha256 *.jar
 openssl dgst -sha256 ../*.zip
 
 # extract TCK in peer directory
-$JAVA_HOME/bin/jar xvf ../jakarta.batch.official.tck-2.0.0-M4.zip
-cd jakarta.batch.official.tck-2.0.0-M4
-
-
+$JAVA_HOME/bin/jar xvf ../$TCK_ARTIFACT_ID.zip
+cd $TCK_ARTIFACT_ID
 
 #------------------------------------------------
 # Run TestNG bucket with properties to configure
 # com.ibm.jbatch implementation
 #------------------------------------------------
 
+echo
+echo --------------------------------
+echo Begin TestNG tests using Java 8
+echo --------------------------------
+echo
+
+
 ant -v -f build.xml -Dbatch.impl.testng.path=../jakarta.batch-api-2.0.0-M6.jar:../com.ibm.jbatch.container-2.0.0-M6.jar:../com.ibm.jbatch.spi-2.0.0-M6.jar:../derby-10.10.1.1.jar  -Djvm.options="-Dcom.ibm.jbatch.spi.ServiceRegistry.BATCH_THREADPOOL_SERVICE=com.ibm.jbatch.container.services.impl.GrowableThreadPoolServiceImpl -Dcom.ibm.jbatch.spi.ServiceRegistry.J2SE_MODE=true -Dcom.ibm.jbatch.spi.ServiceRegistry.CONTAINER_ARTIFACT_FACTORY_SERVICE=com.ibm.jbatch.container.services.impl.DelegatingBatchArtifactFactoryImpl"
 
 
-
+echo
+echo --------------------------------
+echo Begin SigTest setup using Java 8
+echo --------------------------------
+echo
 
 #------------------
 # SIGNATURE TESTS
@@ -127,8 +154,8 @@ cd $TCK_HOME_DIR
 BATCH_API_JAR=$TCK_HOME_DIR/tckdir/prereqs/jakarta.batch-api-2.0.0-M6.jar
 
 IMPL_PATH=$BATCH_API_JAR\
-:$TCK_HOME_DIR/tckdir/prereqs/jakarta.batch.official.tck-2.0.0-M4/lib/jakarta.inject-api-2.0.0-RC4.jar\
-:$TCK_HOME_DIR/tckdir/prereqs/jakarta.batch.official.tck-2.0.0-M4/lib/jakarta.enterprise.cdi-api-3.0.0-M4.jar\
+:$TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/lib/jakarta.inject-api-2.0.0-RC4.jar\
+:$TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/lib/jakarta.enterprise.cdi-api-3.0.0-M4.jar\
 :$TCK_HOME_DIR/tckdir/prereqs/com.ibm.jbatch.container-2.0.0-M6.jar\
 :$TCK_HOME_DIR/tckdir/prereqs/com.ibm.jbatch.spi-2.0.0-M6.jar
 
@@ -136,7 +163,20 @@ IMPL_PATH=$BATCH_API_JAR\
 # Run Java 8 SigTest portion
 # -----------------------------------------------
 
-$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/jakarta.batch.official.tck-2.0.0-M4/artifacts/batch.standalone.tck.sig_2.0_se8   -classpath $JAVA_HOME/jre/lib/rt.jar:$IMPL_PATH
+echo
+echo --------------------------------
+echo Begin SigTest tests using Java 8
+echo --------------------------------
+echo
+
+$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/artifacts/batch.standalone.tck.sig_2.0_se8   -classpath $JAVA_HOME/jre/lib/rt.jar:$IMPL_PATH
+
+echo
+echo -------------------------------------------
+echo At this point you have completed the TCK
+echo running with Java 8
+echo -------------------------------------------
+echo
 
 #------------------------------------------
 # Run SigTest forcing error (not strictly
@@ -148,68 +188,113 @@ echo Exclude CDI API JAR
 echo expecting failure to show tests are working
 echo -------------------------------------------
 echo
-IMPL_PATH=$BATCH_API_JAR\
-:$TCK_HOME_DIR/tckdir/prereqs/jakarta.batch.official.tck-2.0.0-M4/lib/jakarta.inject-api-2.0.0-RC4.jar\
+IMPL_PATH_INCOMPLETE=$BATCH_API_JAR\
+:$TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/lib/jakarta.inject-api-2.0.0-RC4.jar\
 :$TCK_HOME_DIR/tckdir/prereqs/com.ibm.jbatch.container-2.0.0-M6.jar\
 :$TCK_HOME_DIR/tckdir/prereqs/com.ibm.jbatch.spi-2.0.0-M6.jar
 
 # Same command as before, different path
-$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/jakarta.batch.official.tck-2.0.0-M4/artifacts/batch.standalone.tck.sig_2.0_se8   -classpath $JAVA_HOME/jre/lib/rt.jar:$IMPL_PATH
+$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/artifacts/batch.standalone.tck.sig_2.0_se8   -classpath $JAVA_HOME/jre/lib/rt.jar:$IMPL_PATH_INCOMPLETE
 echo
 echo ---------------------
 echo done expected failure
 echo ---------------------
 echo
 
-#---------------
-# Java 11
-#---------------
-
-#---------------------------------------------------------------------------
-# Currently, the Java 11 path works fine, in both the TestNG and SigTest portions of the TCK.
-# The only step missing here is an automated switch between the Java 8 and Java 11 "branches" of this script.
-#---------------------------------------------------------------------------
 
 
-#cd $TCK_HOME_DIR
-#rm -rf sigtest; mkdir -p sigtest/jimage
-#ls -la .
-#JDK11_CLASSES=$TCK_HOME_DIR/sigtest/jimage
-#cd $JDK11_CLASSES
-#
-## Extract here using `jimage extract`
-#$JAVA_HOME/bin/jimage extract $JAVA_HOME/lib/modules
-# Java 11
-#$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/jakarta.batch.official.tck-2.0.0-M4/artifacts/batch.standalone.tck.sig_2.0_se11  -classpath $JDK11_CLASSES/java.base:$IMPL_PATH
+#=============
+# JAVA 11
+#=============
+
+
+echo
+echo --------------------------------
+echo Begin setup using Java 11
+echo --------------------------------
+echo
+
+################
+# OK TO MODIFY
+################
+
+export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-11-openj9
+
+################
+# DO NOT MODIFY
+#  THE REST !
+################
+
+$JAVA_HOME/bin/java -version
+
+# Cleanup TestNG execution dir, but no need to re-copy prereqs.
+cd $TCK_HOME_DIR/tckdir/prereqs
+
+rm -rf $TCK_ARTIFACT_ID
+$JAVA_HOME/bin/jar xvf ../$TCK_ARTIFACT_ID.zip
+cd $TCK_ARTIFACT_ID
 
 
 
-# ------------------------------------------------------------------------------
+#------------------------------------------------
+# Run TestNG bucket with properties to configure
+# com.ibm.jbatch implementation
+#------------------------------------------------
+
+echo
+echo --------------------------------
+echo Begin TestNG tests using Java 11
+echo --------------------------------
+echo
+
+ant -v -f build.xml -Dbatch.impl.testng.path=../jakarta.batch-api-2.0.0-M6.jar:../com.ibm.jbatch.container-2.0.0-M6.jar:../com.ibm.jbatch.spi-2.0.0-M6.jar:../derby-10.10.1.1.jar  -Djvm.options="-Dcom.ibm.jbatch.spi.ServiceRegistry.BATCH_THREADPOOL_SERVICE=com.ibm.jbatch.container.services.impl.GrowableThreadPoolServiceImpl -Dcom.ibm.jbatch.spi.ServiceRegistry.J2SE_MODE=true -Dcom.ibm.jbatch.spi.ServiceRegistry.CONTAINER_ARTIFACT_FACTORY_SERVICE=com.ibm.jbatch.container.services.impl.DelegatingBatchArtifactFactoryImpl"
+
+echo
+echo --------------------------------
+echo Begin SigTest setup using Java 11
+echo --------------------------------
+echo
+
+cd $TCK_HOME_DIR
+JDK11_CLASSES=$TCK_HOME_DIR/jimage-extract
+rm -rf $JDK11_CLASSES; mkdir $JDK11_CLASSES; cd $JDK11_CLASSES
+
+# Extract here using `jimage extract`
+$JAVA_HOME/bin/jimage extract $JAVA_HOME/lib/modules
 
 
+echo
+echo --------------------------------
+echo Begin SigTest tests using Java 11
+echo --------------------------------
+echo
+$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/artifacts/batch.standalone.tck.sig_2.0_se11  -classpath $JDK11_CLASSES/java.base:$IMPL_PATH
 
 
-
-
-
+echo
+echo -------------------------------------------
+echo At this point you have completed the TCK
+echo running with Java 11
+echo -------------------------------------------
+echo
 
 
 #------------------------------------------
 # Run SigTest forcing error (not strictly
-# necessary, but the signature testing is
+# necessary, though the signature testing is
 #------------------------------------------
 echo
 echo -------------------------------------------
+echo Exclude CDI API JAR
 echo expecting failure to show tests are working
 echo -------------------------------------------
 echo
-$JAVA_HOME/bin/java -jar ../sigtestdev-3.0-b12-v20140219.jar SignatureTest -static -package jakarta.batch \
--filename artifacts/batch-api-sigtest-java8.sig \
--classpath ../jakarta.batch-api-2.0.0-M6.jar:$JAVA_HOME/jre/lib/rt.jar:lib/jakarta.inject-api-1.0.jar
-echo
-echo --------------------------------------------
-echo done expecting failure,tests should work now
-echo --------------------------------------------
-echo
 
+# Same command as before, different path
+$JAVA_HOME/bin/java -jar $TCK_HOME_DIR/tckdir/prereqs/sigtestdev-3.0-b12-v20140219.jar   SignatureTest -static -package jakarta.batch -filename  $TCK_HOME_DIR/tckdir/prereqs/$TCK_ARTIFACT_ID/artifacts/batch.standalone.tck.sig_2.0_se11  -classpath $JDK11_CLASSES/java.base:$IMPL_PATH_INCOMPLETE
+echo
+echo ---------------------
+echo done expected failure
+echo ---------------------
+echo
 
