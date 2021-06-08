b2737a305c622ed444a5b783e49524fb463a8055
Scott Kurz <skurz@us.ibm.com>
2021-06-08 15:55:08 -0400

    Update version to 2.1.0-M1-SNAPSHOT
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- com.ibm.jbatch.tck.spi/src/main/java/com/ibm/jbatch/tck/spi/package-info.java
+++ com.ibm.jbatch.tck.spi/src/main/java/com/ibm/jbatch/tck/spi/package-info.java
@@ -32,6 +32,6 @@
  * <p>
  * For more details, see the documentation in the Jakarta Batch TCK Reference Guide, included within the TCK binary.
  *
- * @see <a href="https://download.eclipse.org/jakartaee/batch/2.0/jakarta.batch.official.tck-2.0.0.zip">TCK binary</a>
+ * @see <a href="https://download.eclipse.org/jakartaee/batch/2.1/jakarta.batch.official.tck-2.1.0.zip">TCK binary</a>
  */
 package com.ibm.jbatch.tck.spi;
